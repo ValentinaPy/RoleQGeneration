7ca48b865fe81ab391556c7a9922f12d7947646b
ValentinaPy <valentina@posteo.de>
2021-11-14 12:28:57 +0200

    some fixes to demo

--- bart_transformation/BART_grammar_correction.py
+++ bart_transformation/BART_grammar_correction.py
@@ -1,42 +1,13 @@
-# from transformers import BartForConditionalGeneration, BartTokenizer
-#
-#
-#
-# model = BartForConditionalGeneration.from_pretrained("facebook/bart-large", force_bos_token_to_be_generated=True)
-# tok = BartTokenizer.from_pretrained("facebook/bart-large")
-# # Original question: How isn't the boys being taught ?
-# example_english_phrase = "The boys aren't playing in the garden."
-# question_phrase = "Where <mask> the boys playing?"
-# batch = tok(example_english_phrase, question_phrase, return_tensors='pt')
-# generated_ids = model.generate(batch['input_ids'])
-# print(tok.batch_decode(generated_ids, skip_special_tokens=True))
 import csv
-import sys
+from transformers import pipeline
 
-from transformers import AutoModelWithLMHead, AutoTokenizer, BertForMaskedLM, pipeline
-import torch
-
-# tokenizer = AutoTokenizer.from_pretrained("distilbert-base-cased")
-# model = AutoModelWithLMHead.from_pretrained("distilbert-base-cased")
-# # isn't
-# sequence = f"Where {tokenizer.mask_token} particles dissolved?"
-# input = tokenizer.encode(sequence, return_tensors="pt")
-# mask_token_index = torch.where(input == tokenizer.mask_token_id)[1]
-#
-# token_logits = model(input)[0]
-# mask_token_logits = token_logits[0, mask_token_index, :]
-#
-# top_5_tokens = torch.topk(mask_token_logits, 60, dim=1).indices[0].tolist()
-#
-# for token in top_5_tokens:
-#     print(sequence.replace(tokenizer.mask_token, tokenizer.decode([token])))
 
 def correct_julian_qasrl():
     model = pipeline("fill-mask", model='bert-base-cased', device=0, top_k=100)
-    infile = csv.DictReader(open('/home/nlp/pyatkiv/workspace/CrossSRL/Data/QASRL/qasrl_qanom.filled.train.tsv'), delimiter='\t')
+    infile = csv.DictReader(open('qasrl_qanom.filled.train.tsv'), delimiter='\t')
     header = list(infile.fieldnames)
     header.append('fixed_question')
-    outfile = csv.DictWriter(open('/home/nlp/pyatkiv/workspace/CrossSRL/Data/QASRL/qasrl_qanom.filled.corrected.train.tsv', 'w'), delimiter='\t', fieldnames=header)
+    outfile = csv.DictWriter(open('qasrl_qanom.filled.corrected.train.tsv', 'w'), delimiter='\t', fieldnames=header)
     outfile.writeheader()
     pairs_dict = {'does': 'do', 'do': 'does', 'is': 'are', 'are': 'is', 'has':'have', 'have':'has', 'was':'were', 'were':'was'}
     negation_dict = {"doesn't": "don't", "don't": "doesn't", "isn't": "aren't", "aren't": "isn't", "hasn't":"haven't", "haven't":"hasn't", "wasn't":"weren't", "weren't":"wasn't"}
